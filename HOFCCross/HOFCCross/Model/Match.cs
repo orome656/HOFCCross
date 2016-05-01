03a55382f27c9e4ad282ec75cfe17ee796b8dc11
orome656 <anthony.maladot@gmail.com>
2016-05-01 23:37:24 +0200

    Add models and mock service

--- HOFCCross/HOFCCross/Model/Match.cs
+++ HOFCCross/HOFCCross/Model/Match.cs
@@ -8,5 +8,11 @@ namespace HOFCCross.Model
 {
     class Match
     {
+        public string Equipe1 { get; set; }
+        public string Equipe2 { get; set; }
+        public int? Score1 { get; set; }
+        public int? Score2 { get; set; }
+        public DateTime Date { get; set; }
+        public Competition Competition { get; set; }
     }
 }
--- HOFCCross/HOFCCross/Service/MockService.cs
+++ HOFCCross/HOFCCross/Service/MockService.cs
@@ -14,25 +14,100 @@ namespace HOFCCross.Service
             List<Actu> actus = new List<Actu>();
             actus.Add(new Actu()
             {
-                Title = "Test 1",
-                Content = "Test 1"
+                Title = "HOFC III – MARQUISAT II en images",
+                Content = "Merci à Jean-Paul LAMARQUE CHOY pour les photos",
+                Url = "http://www.hofc.fr/2016/03/hofc-iii-marquisat-ii-en-images/",
+                ImageUrl = "http://www.hofc.fr/wp-content/themes/Canyon/timthumb.php?src=http://www.hofc.fr/wp-content/uploads/2016/02/HOFC-SEN-HOFC-III-1-3-MARQUISAT-II-20-02-2016-8.jpg&h=150&w=250&zc=1"
             });
             actus.Add(new Actu()
             {
-                Title = "Test 1",
-                Content = "Test 1"
+                Title = "HOFC III – MARQUISAT II en images",
+                Content = "Merci à Jean-Paul LAMARQUE CHOY pour les photos",
+                Url = "http://www.hofc.fr/2016/03/hofc-iii-marquisat-ii-en-images/",
+                ImageUrl = "http://www.hofc.fr/wp-content/themes/Canyon/timthumb.php?src=http://www.hofc.fr/wp-content/uploads/2016/02/HOFC-SEN-HOFC-III-1-3-MARQUISAT-II-20-02-2016-8.jpg&h=150&w=250&zc=1"
             });
             actus.Add(new Actu()
             {
-                Title = "Test 1",
-                Content = "Test 1"
+                Title = "HOFC III – MARQUISAT II en images",
+                Content = "Merci à Jean-Paul LAMARQUE CHOY pour les photos",
+                Url = "http://www.hofc.fr/2016/03/hofc-iii-marquisat-ii-en-images/",
+                ImageUrl = "http://www.hofc.fr/wp-content/themes/Canyon/timthumb.php?src=http://www.hofc.fr/wp-content/uploads/2016/02/HOFC-SEN-HOFC-III-1-3-MARQUISAT-II-20-02-2016-8.jpg&h=150&w=250&zc=1"
             });
             return actus;
         }
 
-        public List<Match> GetCalendrier()
+        public List<Match> GetMatchs()
         {
-            throw new NotImplementedException();
+            List<Match> matchs = new List<Match>();
+
+            matchs.Add(new Match()
+            {
+                Date = DateTime.Now,
+                Equipe1 = "Test1",
+                Equipe2 = "Test2",
+                Competition = new Competition()
+                {
+                    Nom = "Excellence",
+                    Categorie = "equipe1",
+                    IsChampionnat = true
+                }
+            });
+
+            matchs.Add(new Match()
+            {
+                Date = DateTime.Now,
+                Equipe1 = "Test1",
+                Equipe2 = "Test2",
+                Score1 = 1,
+                Score2 = 1,
+                Competition = new Competition()
+                {
+                    Nom = "Excellence",
+                    Categorie = "equipe1",
+                    IsChampionnat = true
+                }
+            });
+
+            matchs.Add(new Match()
+            {
+                Date = DateTime.Now,
+                Equipe1 = "Test1",
+                Equipe2 = "Test2",
+                Competition = new Competition()
+                {
+                    Nom = "Autre",
+                    Categorie = "equipe1",
+                    IsChampionnat = false
+                }
+            });
+
+            matchs.Add(new Match()
+            {
+                Date = DateTime.Now,
+                Equipe1 = "Test1",
+                Equipe2 = "Test2",
+                Competition = new Competition()
+                {
+                    Nom = "Autre",
+                    Categorie = "equipe2",
+                    IsChampionnat = true
+                }
+            });
+
+            matchs.Add(new Match()
+            {
+                Date = DateTime.Now,
+                Equipe1 = "Test1",
+                Equipe2 = "Test2",
+                Competition = new Competition()
+                {
+                    Nom = "Autre",
+                    Categorie = "equipe2",
+                    IsChampionnat = false
+                }
+            });
+
+            return matchs;
         }
     }
 }
