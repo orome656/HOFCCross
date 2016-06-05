35e1a6ab6049a70c412be26cb3c92ac6e0244878
orome656 <anthony.maladot@gmail.com>
2016-06-05 22:06:57 +0200

    Create Match Control template

---
+++ HOFCCross/HOFCCross/Controls/Match.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace HOFCCross.Controls
+{
+    public partial class Match : ContentView
+    {
+        public Match()
+        {
+            InitializeComponent();
+        }
+    }
+}
