c7ad4221ce990bb0b4cad325199cb98f7fc7dfad
Anthony Maladot <anthony.maladot@gmail.com>
2016-06-20 14:07:07 +0200

    Constant value correction

--- HOFCCross/HOFCCross/Constantes/AppConstantes.cs
+++ HOFCCross/HOFCCross/Constantes/AppConstantes.cs
@@ -8,7 +8,7 @@ namespace HOFCCross.Constantes
 {
     public class AppConstantes
     {
-        public static readonly string HOFC_NAME = "	HORGUES ODOS";
+        public static readonly string HOFC_NAME = "HORGUES ODOS";
         public static readonly string SERVER_BASE_URL = "http://hofcweb.cloudapp.net/";
         public static readonly string SERVER_ACTU_URL = SERVER_BASE_URL + "api/Actu";
         public static readonly string SERVER_MATCH_URL = SERVER_BASE_URL + "api/Match";
