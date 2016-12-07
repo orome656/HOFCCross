bcd64e1e895bda815764fb590ff1eb5c4879d504
orome656 <anthony.maladot@gmail.com>
2016-12-07 14:40:41 +0100

    UWP Database Implementation

---
+++ HOFCCross/HOFCCross.UWP/Database/SQLiteUWP.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+using Windows.Storage;
+using System.IO;
+using Xamarin.Forms;
+using HOFCCross.UWP.Database;
+
+[assembly: Dependency(typeof(SQLiteUWP))]
+namespace HOFCCross.UWP.Database
+{
+    public class SQLiteUWP : HOFCCross.Database.Database
+    {
+        protected override SQLiteAsyncConnection CreateConnection()
+        {
+            var databasePath = Path.Combine(ApplicationData.Current.LocalFolder.Path, "HOFC.db3");
+            return new SQLiteAsyncConnection(databasePath);
+        }
+    }
+}
