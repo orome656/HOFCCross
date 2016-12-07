e20c49d84e48a279642db3bc09b02a8e20feb2a9
orome656 <anthony.maladot@gmail.com>
2016-12-07 14:26:56 +0100

    Ranking view design

---
+++ HOFCCross/HOFCCross/Converters/ColorIfHOFCConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace HOFCCross.Converters
+{
+    public class ColorIfHOFCConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            IsHOFCConverter converter = new Converters.IsHOFCConverter();
+            return (bool)converter.Convert(value, targetType, parameter, culture) ? Color.FromHex("#08589D") : Color.Default;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
